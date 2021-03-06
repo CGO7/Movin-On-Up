5f426bf5d38e1a330bf04f30e60cd3a0af95cbb5
Chris Goslin <cgoslin7@gmail.com>
2021-03-06 12:56:44 -0600

    Added the Weather API to the javascript

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -14,3 +14,27 @@ $.ajax(settings).done(function (response) {
 	console.log(response);
 });
 
+const settingsWeather = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://us-weather-by-city.p.rapidapi.com/getweather?city=San%20Francisco&state=CA",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "cc3bdfe5d0mshc16b581a0701a7ep103683jsn6aabd3b9dcb2",
+		"x-rapidapi-host": "us-weather-by-city.p.rapidapi.com"
+	}
+};
+
+$.ajax(settingsWeather).done(function (response) {
+	console.log(response);
+});
+
+
+
+
+//need event listener to search button to trigger API codes
+
+//need to json parse array to get list, with query selector for list to land on
+
+//
+
